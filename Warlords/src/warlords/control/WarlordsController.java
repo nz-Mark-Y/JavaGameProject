ec8968f2a409c23adb7a27ff1bbb299f5ed6a619
Mark Yep <myep112@aucklanduni.ac.nz>
2017-03-24 12:21:57 +1300

    fixed up gui

--- Warlords/src/warlords/control/WarlordsController.java
+++ Warlords/src/warlords/control/WarlordsController.java
@@ -4,20 +4,37 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class WarlordsController extends Application {
+	private Stage primaryStage;
 	private BorderPane rootLayout;
 	
 	@Override
-	public void start(Stage primaryStage) throws IOException {
-		FXMLLoader loader= new FXMLLoader();
-		loader.setLocation(WarlordsController.class.getResource("view/GameView.fxml"));
-		this.rootLayout = (BorderPane) loader.load();
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Warlords");
+		
+		initRootLayout();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(WarlordsController.class.getResource("../view/GameView.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene  = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
