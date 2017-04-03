68f7df80f612b0f9845aa3f206c87070cf775493
Sylvain Bechet <bechet.sylvain@gmail.com>
2017-04-04 10:35:14 +1200

    Added Warlord and Paddle Patterns
    Included all sprites as part of the project and moved the warlords in
    appropriate places on the scene.

--- Warlords/src/warlords/WarlordsController.java
+++ Warlords/src/warlords/WarlordsController.java
@@ -40,10 +40,10 @@ public class WarlordsController extends Application {
 	// Set up the objects in a new game
 	public void createNewGame() {
 		Ball ball = new Ball(350, 350);
-		Warlord player0 = new Warlord(new Paddle(181, 547), 100, 668);
-		Warlord player1 = new Warlord(new Paddle(181, 181), 100, 100);
-		Warlord player2 = new Warlord(new Paddle(547, 181), 668, 100);
-		Warlord player3 = new Warlord(new Paddle(547, 547), 668, 668);
+		Warlord player0 = new Warlord(new Paddle(181, 547), 40, 678);
+		Warlord player1 = new Warlord(new Paddle(181, 181), 40, 40);
+		Warlord player2 = new Warlord(new Paddle(547, 181), 678, 40);
+		Warlord player3 = new Warlord(new Paddle(547, 547), 678, 678);
 		ArrayList<Warlord> playerList = new ArrayList<Warlord>();
 		playerList.add(player0);
 		playerList.add(player1);
