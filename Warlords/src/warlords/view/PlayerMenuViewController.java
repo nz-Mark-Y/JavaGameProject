93108fc7cc0fac92488eda2f4c68e46c316a3051
Mark Yep <mark_yep@hotmail.com>
2017-04-13 10:42:03 +1200

    small fixes

--- Warlords/src/warlords/view/PlayerMenuViewController.java
+++ Warlords/src/warlords/view/PlayerMenuViewController.java
@@ -436,10 +436,10 @@ public class PlayerMenuViewController {
 		descriptions.add("New Zealand: \n You have an extra sheep paddle guarding your warlord \n It wanders randomly around the same path as your paddle. \n Baaa!");
 		descriptions.add("China: \n You have two extra walls guarding your warlord. Reliability not guaranteed.");
 		descriptions.add("Australia: \n Shoots a spider using your UP key \n Spider will not affect walls or warlords, but if its hits a paddle it will be slowed for 5s. 20s cooldown.");
-		descriptions.add("India: \n To Be Implemented");
+		descriptions.add("India: \n Unlocked in DLC");
 		descriptions.add("Russia: \n Sieze the Memes of Production! \n Press UP to take random walls from other players' bases and adds them to a communal wall in the middle. Single use.");
 		descriptions.add("Egypt: \n Press UP to turn your paddle into a massive pyramid for 5s. \n It can't move during this time but is immune to slow effects. 20s cooldown.");
-		descriptions.add("Brazil: \n To Be Implemented");
+		descriptions.add("Brazil: \n Unlocked in DLC");
 	}
 	
 	// Text
