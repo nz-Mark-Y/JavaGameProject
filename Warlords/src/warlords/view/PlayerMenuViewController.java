a6c341da0aa0c0ef75d58bcab241f9589cb30733
Sylvain Bechet <bechet.sylvain@gmail.com>
2017-04-12 12:27:40 +1200

    Egypt Description Fix

--- Warlords/src/warlords/view/PlayerMenuViewController.java
+++ Warlords/src/warlords/view/PlayerMenuViewController.java
@@ -439,7 +439,7 @@ public class PlayerMenuViewController {
 		descriptions.add("Australia: \n Shoots a spider using your UP key \n Spider will not affect walls or warlords, but if its hits a paddle it will be slowed for 5s. 20s cooldown.");
 		descriptions.add("India: \n To Be Implemented");
 		descriptions.add("Russia: \n Sieze the Memes of Production! \n Press UP to take random walls from other players' bases and adds them to a communal wall in the middle. Single use.");
-		descriptions.add("Egypt: \n Press UP to turn your paddle into a massive pyramid for 5s. /n It can't move during this time but is immune to slow effects. 20s cooldown.");
+		descriptions.add("Egypt: \n Press UP to turn your paddle into a massive pyramid for 5s. \n It can't move during this time but is immune to slow effects. 20s cooldown.");
 		descriptions.add("Brazil: \n To Be Implemented");
 	}
 	
